6db16f0bec110ea40f224b9c16129328060b0b2a
atsuushi <atsuushi@GMail.com>
2010-11-05 00:56:32 +0900

    added Bshell script for Unix-like environment

---
+++ uurss.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+## set JDBCMODE if you use JDBC mode.
+# export JDBCMODE=true
+
+CP=uurss.jar:lib/rome-1.0.jar:lib/log4j-1.2.15.jar:lib/velocity-dep-1.5.jar:lib/jdom-1.0.jar:${CLASSPATH}
+OP=-Dresult.dir=result
+
+if [ "${JDBCMODE}" = "true" ]; then
+	CP=${CP}:lib/sqlite-jdbc-3.6.13.jar
+	OP=${OP} -Djdbc.drivers=org.sqlite.JDBC \
+             -Dconnection.url=jdbc:sqlite:db/feedlist.sqlite \
+             -Dconnection.user= -Dconnection.password=
+else
+	OP="${OP} -Dcsv=db/feedlist.csv"
+fi
+
+java -cp "${CP}" ${OP} uurss.Main $*
