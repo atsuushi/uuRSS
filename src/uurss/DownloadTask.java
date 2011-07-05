a7d23a5de710f257911486d00ddeccc5549c292e
atsuushi <atsuushi@gmail.com>
2011-07-05 23:57:02 +0900

    decompresses feed data if it is gzipped

--- src/uurss/DownloadTask.java
+++ src/uurss/DownloadTask.java
@@ -5,6 +5,7 @@ import java.net.*;
 import java.nio.channels.*;
 import java.security.*;
 import java.util.concurrent.*;
+import java.util.zip.*;
 
 import org.apache.log4j.*;
 
@@ -42,20 +43,61 @@ final class DownloadTask implements Callable<File> {
         URL url = new URL(info.url);
         InputStream is = url.openStream();
         try {
-            FileOutputStream fos = new FileOutputStream(feedFile);
-            try {
-                final FileChannel wch = fos.getChannel();
-                final ReadableByteChannel rch = Channels.newChannel(is);
-                for (long p = 0, read = 0; (read = wch.transferFrom(rch, p, 8192)) > 0; p += read) {
-                    //
-                }
-            } finally {
-                fos.close();
-            }
+            transfer(is, feedFile);
         } finally {
             is.close();
         }
+        if (isGzipFile(feedFile)) {
+            if (log.isDebugEnabled()) {
+                log.debug(String.format("unzip [%s]", feedFile.getName()));
+            }
+            File tmp = new File(root, ".tmp.dat");
+            // unzip
+            InputStream gis = new GZIPInputStream(new FileInputStream(feedFile));
+            try {
+                transfer(gis, tmp);
+            } finally {
+                gis.close();
+            }
+            if (feedFile.delete() && !tmp.renameTo(feedFile)) {
+                log.warn(String.format("failed to rename [%s] to [%s]",
+                                       tmp.getName(),
+                                       feedFile.getName()));
+            }
+        }
         return feedFile;
+    }
+
+    private static boolean isGzipFile(File file) throws IOException {
+        if (file.length() >= 2) {
+            InputStream fis = new FileInputStream(file);
+            try {
+                byte[] bytes = new byte[2];
+                if (fis.read(bytes) >= 2) {
+                    if (((bytes[1] & 0xFF) * 0x100 + (bytes[0] & 0xFF)) == GZIPInputStream.GZIP_MAGIC) {
+                        return true;
+                    }
+                }
+            } finally {
+                fis.close();
+            }
+        }
+        return false;
+    }
+
+    private static long transfer(InputStream is, File dst) throws IOException {
+        FileOutputStream fos = new FileOutputStream(dst);
+        try {
+            final FileChannel wch = fos.getChannel();
+            final ReadableByteChannel rch = Channels.newChannel(is);
+            long p = 0L;
+            for (long read = 0L; (read = wch.transferFrom(rch, p, 8192)) > 0; p += read) {
+                //
+            }
+            return p;
+        } finally {
+            fos.close();
+        }
     }
 
     private static String getMessageDigestString(String input) {
